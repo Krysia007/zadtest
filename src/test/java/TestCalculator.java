becff270d6b75b9290ff76bac8e75c45477b6e24
Krysia007 <k_trojanowska@go2.pl>
2018-04-27 19:50:46 +0200

    implementacja dla klasy StringCalculator

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,9 +1,16 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestCalculator {
 
     StringCalculator calc;
+
+    @Before
+    public void setupTest () {
+        calc = new StringCalculator();
+    }
+
     @Test
     public void testEmptyString() {
         int result = calc.add("");
@@ -11,11 +18,11 @@ public class TestCalculator {
     }
      @Test
      public void TestOneEmpty() {
-            int result = calc.add("");
+            int result = calc.add("7");
             assertEquals(7, result);
         }
      @Test
-     public void TestOneEmpty() {
+     public void TestTwoEmpty() {
                 int result = calc.add("3+5");
                 assertEquals(8,result);
     }
